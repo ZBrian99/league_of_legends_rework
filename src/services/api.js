81685aac844f550507212e615ed6fac737ee891a
ZBrian199 <brianzelada1999@gmail.com>
2023-03-01 10:11:39 -0300

    fix api

--- src/services/api.js
+++ src/services/api.js
@@ -1,48 +1,11 @@
-
 export const getChampions = async () => {
 	const resp = await fetch('https://ddragon.leagueoflegends.com/cdn/12.23.1/data/en_US/champion.json');
 	const { data } = await resp.json();
 	return data;
 };
 
-export const getChampionById = async (chamName) => {
-	const resp = await fetch(
-		`https://ddragon.leagueoflegends.com/cdn/12.23.1/data/en_US/champion/${chamName}.json`
-	);
+export const getChampionById = async (id) => {
+	const resp = await fetch(`https://ddragon.leagueoflegends.com/cdn/12.23.1/data/en_US/champion/${id}.json`);
 	const { data } = await resp.json();
 	return data;
 };
-
-// export const getChampionsExtra = async () => {
-// 	const resp = await fetch(`../data/extra-cham-info.json`);
-// 	// const resp = await fetch(`../../src/data/extra-cham-info.json`);
-// 	const {data} = await resp.json();
-//   // console.log(data);
-// 	return data;
-// };
-
-
-
-
-// import axios from 'axios';
-
-// const useApi = (url) => {
-// 	const [data, setData] = useState(null);
-
-// 	const fetchData = async () => {
-// 		try {
-// 			const response = await axios.get(url);
-// 			setData(response.data);
-// 		} catch (error) {
-// 			console.log(error);
-// 		}
-// 	};
-
-// 	useEffect(() => {
-// 		fetchData();
-// 	}, []);
-
-// 	return data; // returns the data from the API call as a state variable.
-// };
-
-// export default useApi;
