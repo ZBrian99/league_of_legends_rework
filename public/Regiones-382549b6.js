0409b7d2b0f92b04961884001ff3beb1db089c1e
ZBrian199 <brianzelada1999@gmail.com>
2023-03-10 12:40:17 -0300

    Updates

---
+++ public/Regiones-382549b6.js
@@ -0,0 +1,1 @@
+import{b as e}from"./main-b2e2d283.js";const r=()=>e("div",{children:"Regiones"});export{r as Regiones};
---
+++ public/Universo-6de269af.js
@@ -0,0 +1,1 @@
+import{b as r}from"./main-b2e2d283.js";const e=()=>r("div",{children:"Universo"});export{e as Universo};
